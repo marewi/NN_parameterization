47c071cce98a1656cd2be1325a7a0ba28e4dabc2
Marc Wittlinger <marc-wittlinger@gmx.de>
2020-01-21 22:51:10 +0000

    restoring

--- parameters.py
+++ parameters.py
@@ -5,6 +5,20 @@ num_epochs_stepsize = 1
 batch_size_stepsize = 1
 learning_rate_stepsize = 0.001
 
+# barriers of parameters
+num_epochs_min = 1
+num_epochs_max = 10
+num_epochs_min = 10
+num_epochs_max = 20
+
+batch_size_min = 1
+batch_size_max = 10
+batch_size_min = 10
+batch_size_max = 20
+
+learning_rate_min = 0
+learning_rate_max = 0.5
+
 
 ################################
 ### LEARNING PARAMETERS
